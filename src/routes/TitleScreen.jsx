68f3a933259e819f0ac5cb69874eb74511fb660e
AtanasovCode <a_ljupce@yahoo.com>
2024-11-12 18:02:18 +0100

    installed Zustand and crated Zustand store

--- src/routes/TitleScreen.jsx
+++ src/routes/TitleScreen.jsx
@@ -1,9 +1,14 @@
-
+import { useCardStore } from "../../useCardStore";
 
 const TitleScreen = () => {
+
+    const { bears } = useCardStore();
+
     return (
         <div className="min-h-screen bg-black text-white">
             Title Screen
+
+            {bears}
         </div>
     );
 }
