3e70b2e73c854d6a99d0fc8adf5a5ad8fe60d2bb
AtanasovCode <a_ljupce@yahoo.com>
2024-11-21 13:05:07 +0100

    added game description & how to play explanation

--- src/components/PlayDescription.jsx
+++ src/components/PlayDescription.jsx
@@ -1,8 +1,37 @@
 
 
-const PlayDescription = () => {
+const PlayDescription = ({
+    title,
+    icon,
+    iconMobile,
+    description,
+    image,
+}) => {
+
+    const showIcon = iconMobile ? "hidden" : "flex";
+
     return (
-        <div></div>
+        <div className="w-full flex flex-col items-center justify-center mb-12">
+            <div className="w-full flex items-center justify-start gap-2 mb-4">
+                <div className={`${showIcon} lg:flex items-center justify-center w-10 md:w-12 lg:w-14`}>
+                    {icon}
+                </div>
+                {
+                    iconMobile && <div className={`lg:hidden flex items-center justify-center w-10`}>
+                        {iconMobile}
+                    </div>
+                }
+                <div className="text-lg font-semibold md:text-xl lg:text-xl">
+                    {title}
+                </div>
+            </div>
+            <div className="text-sm text-slate-300 text-left w-full">
+                {description}
+            </div>
+            <div className="w-full flex items-center justify-center">
+                {image && <img src={image} alt="descriptive image" className="w-full" />}
+            </div>
+        </div>
     );
 }
 
--- src/routes/HowToPlay.jsx
+++ src/routes/HowToPlay.jsx
@@ -1,21 +1,48 @@
-import { 
+import {
     Strategy,
     ShuffleAngular,
     MouseLeftClick,
     HandTap,
     Trophy,
+    DiceFive,
+    FlagCheckered,
 } from "@phosphor-icons/react";
+
+import PlayDescription from "../components/PlayDescription";
 
 const HowToPlay = () => {
     return (
-        <div className="min-h-[100dvh] bg-main-background text-white flex flex-col items-center justify-start py-8">
-            <div className="flex items-center justify-center gap-2 mb-12">
-                <div className="w-10 flex items-center justify-center">
-                    <Strategy size="100%" weight="fill" color="#FFF" />
+        <div className="min-h-[100dvh] bg-main-background text-white flex items-center justify-center py-8">
+            <div className="w-[90%] sm:w-[80%] md:w-[70%] lg:w-[65%] xl:w-[50%] xl:max-w-[100rem] flex flex-col items-center justify-center">
+                <div className="w-full flex items-center justify-center gap-2 mb-12">
+                    <div className="w-10 md:w-14 lg:w-16 flex items-center justify-center">
+                        <Strategy size="100%" weight="fill" color="#FFF" />
+                    </div>
+                    <div className="font-bold text-xl md:text-2xl lg:text-3xl">
+                        How To Play
+                    </div>
                 </div>
-                <div className="font-bold text-xl">
-                    How To Play
-                </div>
+                <PlayDescription
+                    icon={<DiceFive weight="fill" color="#FFF" size="100%" />}
+                    iconMobile={null}
+                    title="Randomized cards"
+                    description="Each game starts with 3 unique, randomized playing cards. Every round, the cards are shuffled and increase in number, testing your memory skills."
+                    image={null}
+                />
+                <PlayDescription
+                    icon={<MouseLeftClick size="100%" weight="fill" color="#FFF" />}
+                    iconMobile={<HandTap size="100%" weight="fill" color="#FFF" />}
+                    title="Click each card only once"
+                    description="Your goal is to click on every card exactly once. Clicking on the same card twice ends the game, so focus and remember your choices!"
+                    image={null}
+                />
+                <PlayDescription
+                    icon={<FlagCheckered size="100%" weight="fill" color="#FFF" />}
+                    title="Progress through rounds"
+                    description="Complete a round by clicking all unique cards without repeating. Each new round introduces an additional card, challenging your memory as you advance."
+                    image={null}
+                />
+
             </div>
         </div>
     );
